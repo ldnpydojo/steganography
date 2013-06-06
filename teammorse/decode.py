54bdf2c90aa4d5e44144240f5d93ba8a68b1b01b
Salim Fadhley <sal@stodge.org>
2013-06-06 21:10:00 +0100

    First pass at a decoder

--- teammorse/decode.py
+++ teammorse/decode.py
@@ -1,12 +1,29 @@
-import StringIO
+import sys
+from constants import WHITESPACE_TO_NUMERALS
+from constants import LETTERS
 
-
-def generateLines(inputText):
-	for line in StringIO.StringIO(inputText):
+def generateLines(inputFile):
+	for line in inputFile:
 		if line.strip() == '':
 			yield line
 
-def decode()
+def convert(inputLine):
+	binaryChars = []
+	for char in inputLine:
+		binaryChars.append(WHITESPACE_TO_NUMERALS[char])
 
+	charString = ''.join(binaryChars)
+	hiddenNumber = int(charString, 2)
+	return LETTERS[hiddenNumber]
 
+def generateLetters(inputText):
+	for line in generateLines(inputText):
+		yield convert(line)
 
+def decode(inputFile):
+	return ''.join(generateLetters(inputFile))
+
+if __name__ == '__main__':
+	inputFileName = sys.argv[1]
+	with open(inputFileName) as inputFile:
+		print(decode(inputFile))
