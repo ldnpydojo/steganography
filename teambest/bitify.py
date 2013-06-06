9f3542a2953fff6846b13e1add5695e727898e6b
Fran Hermoso <franh@giffgaff.co.uk>
2013-06-06 21:07:55 +0100

    add bitify

---
+++ teambest/bitify.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import json
+
+def bitify(x):
+	s=json.dumps(x)
+	for i in bytearray(s):
+		for j in range(8):
+			yield i & 1
+			i >>= 1
+
+def unbitify(generator):
+	result = []
+	for bit_tuple in zip(*[iter(generator)] * 8):
+		byte=0
+		for bit in bit_tuple:
+			byte >>= 1
+			byte |= bit << 7
+		result.append(chr(byte))
+	return json.loads("".join(result))
+
